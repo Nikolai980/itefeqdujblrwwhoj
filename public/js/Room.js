5e9396751a30857a64045eab4014440c7dac2361
R4 Club <74131129+Nikolai980@users.noreply.github.com>
2023-04-18 20:16:17 +0200

    Update Room.js

--- public/js/Room.js
+++ public/js/Room.js
@@ -26,7 +26,7 @@ const socket = io({ transports: ['websocket'] });
 const surveyActive = true;
 
 const url = {
-    survey: 'https://www.questionpro.com/t/AUs7VZq02P',
+    survey: 'https://questionpro.com/',
 };
 
 const _PEER = {
@@ -486,7 +486,7 @@ function whoAreYou() {
         allowOutsideClick: false,
         allowEscapeKey: false,
         background: swalBackground,
-        title: 'MiroTalk SFU',
+        title: 'AvtoStart SFU',
         input: 'text',
         inputPlaceholder: 'Enter your name',
         inputValue: default_name,
@@ -2382,11 +2382,11 @@ function showAbout() {
         html: `
         <br/>
         <div id="about">
-            <b><a href="https://github.com/miroslavpejic85/mirotalksfu" class="umami--click--github" target="_blank">Open Source</a></b> project
+            <b><a href="https://github.com/avtostart" class="umami--click--github" target="_blank">Private Source</a></b> project
             <br/><br />
-            <button class="pulsate umami--click--sponsors" onclick="window.open('https://github.com/sponsors/miroslavpejic85?o=esb')"><i class="fas fa-heart"></i> Support</button>
+            <button class="pulsate umami--click--sponsors" onclick="window.open('https://github.com/sponsors/r4club?o=esb')"><i class="fas fa-heart"></i> Support</button>
             <br /><br />
-            Author: <a href="https://www.linkedin.com/in/miroslav-pejic-976a07101/" class="umami--click--linkedin" target="_blank"> Miroslav Pejic</a>
+            Author: <a href="https://www.linkedin.com/in/r4club/" class="umami--click--linkedin" target="_blank"> R4 Club</a>
         </div>
         `,
         showClass: {
